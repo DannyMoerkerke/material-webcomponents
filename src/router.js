79ba7da13c00b0121dae6069bf1aea8bbf16df8f
Danny Moerkerke <danny@dannymoerkerke.nl>
2018-01-02 22:54:10 +0100

    router

--- src/router.js
+++ src/router.js
@@ -1,20 +1,41 @@
 export default (outlet, routes) => {
+    const getCurrentRoute = url => url.replace(location.origin, '');
+
+    const getMatchedRoute = url => routes.find(route => url === route.url);
+
+    const matchRoute = url => {
+        const currentRoute = getCurrentRoute(url);
+        const matchedRoute = getMatchedRoute(currentRoute);
+
+        if(matchedRoute) {
+            const {template, url} = matchedRoute;
+            history.pushState({template, url}, null, url);
+
+            activateRoute(matchedRoute);
+        }
+    };
+
+    const activateRoute = ({template, controller}) => {
+        outlet.innerHTML = template;
+
+        if(controller && typeof controller === 'function') {
+            controller();
+        }
+    };
 
     document.querySelectorAll('a').forEach(link => {
-
         link.onclick = e => {
             e.preventDefault();
 
-            const matchedRoute = routes.find(route => e.target.href === `${location.href}${route.url}`);
-
-            if(matchedRoute) {
-                const template = matchedRoute.template(matchedRoute.data);
-                console.log(template);
-                outlet.innerHTML = template;
-                history.pushState({template}, null, matchedRoute.url);
-
-                // matchedRoute.controller();
-            }
+            matchRoute(e.target.href);
         };
-    })
+    });
+
+    window.addEventListener('popstate', e => {
+        const matchedRoute = getMatchedRoute(e.state.url);
+
+        activateRoute(matchedRoute);
+    });
+
+    matchRoute(location.href);
 };
