25d2841482aa64b70cdd17de4a1a4e8b46e25e06
Danny Moerkerke <danny@dannymoerkerke.nl>
2018-02-01 22:15:57 +0100

    - added material-tabs - added tests

---
+++ src/material-tabs.js
@@ -0,0 +1,160 @@
+export default class MaterialTabs extends HTMLElement {
+
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({mode: 'open'});
+
+        shadowRoot.innerHTML = `
+            <style>
+                :host {
+                    --tabs-background: #999999;
+                    --indicator-color: #ffff00;
+                    --content-background: #cccccc;
+                    --content-width: 0;
+                    --content-container-width: 0;
+                }
+                #container {
+                    display: grid;
+                    grid-template-columns: 1fr;
+                    grid-template-rows: 48px 1fr;
+                }
+            
+                #tab-container {
+                    display: flex;
+                    position: relative;
+                    background: var(--tabs-background);
+                    overflow: auto;
+                }
+                #content-container {
+                    width: var(--content-container-width);
+                    height: 50px;
+                    max-height: 50px;
+                    overflow: hidden;
+                }
+                .slide {
+                    transition: margin-left .4s cubic-bezier(0, 0.92, 0.32, 0.98);
+                }
+                #tab-content {
+                    background: var(--content-background);
+                    height: 50px;
+                    overflow: hidden;
+                }
+                ::slotted([slot="tab"]) {
+                    padding: 20px;
+                    float: left;
+                    width: var(--content-width);
+                    box-sizing: border-box;
+                }
+                .tab {
+                    display: flex;
+                    box-sizing: border-box;
+                    align-items: center;
+                    justify-content: center;
+                    min-width: 160px;
+                    max-width: 264px;
+                    height: 48px;
+                    padding-left: 24px;
+                    padding-right: 24px;
+                    flex-grow: 1;
+                    text-transform: uppercase;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    cursor: pointer;
+                }
+                #active-indicator {
+                    position: absolute;
+                    left: var(--indicator-left, 0);
+                    bottom: 0;
+                    width: var(--indicator-width);
+                    border-bottom: 2px solid var(--indicator-color);
+                    transition: left .4s cubic-bezier(0, 0.92, 0.32, 0.98);
+                }
+            </style>
+            
+            <div id="container">
+                <div id="tab-container">
+                    <div id="active-indicator"></div>
+                </div>
+                <div id="tab-content">
+                    <div id="content-container">
+                        <slot name="tab"></slot>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    connectedCallback() {
+        this.curIndex = 0;
+        this.tabContainer = this.shadowRoot.querySelector('#tab-container');
+        this.contentContainer = this.shadowRoot.querySelector('#content-container');
+        this.tabContent = this.shadowRoot.querySelector('#tab-content');
+        this.host = this.tabContainer.getRootNode().host;
+        const {left} = this.tabContainer.getBoundingClientRect();
+        this.tabContainerLeft = left;
+        this.indicator = this.shadowRoot.querySelector('#active-indicator');
+        this.contentDivs = this.shadowRoot.querySelector('slot[name="tab"]').assignedNodes();
+
+        this.contentDivs.forEach((contentDiv, index) => {
+            const tab = document.createElement('div');
+            tab.classList.add('tab');
+            tab.dataset.index = index;
+            tab.textContent = contentDiv.dataset.title;
+
+            this.tabContainer.insertBefore(tab, this.indicator);
+        });
+
+        this.tabs = [...this.shadowRoot.querySelectorAll('.tab')];
+        // this.contentDivs[0].classList.add('active');
+        this.tabs[0].classList.add('active');
+
+        const {width} = this.tabs[0].getBoundingClientRect();
+
+        this.indicator.style.setProperty('--indicator-width', `${width}px`);
+        this.host.style.setProperty('--content-width', `${this.tabContainer.offsetWidth}px`);
+        this.host.style.setProperty('--content-container-width', `${this.tabContainer.offsetWidth * this.tabs.length}px`);
+
+        if(this.hasAttribute('slide')) {
+            this.contentContainer.classList.add('slide')
+        }
+
+        this.setupEventHandlers();
+    }
+
+    setupEventHandlers() {
+        this.tabContainer.addEventListener('click', this.handleClick.bind(this));
+
+        window.addEventListener('resize', () => {
+            const {width} = this.shadowRoot.querySelector('.tab').getBoundingClientRect();
+            const {left} = this.shadowRoot.querySelector('.active').getBoundingClientRect();
+
+            this.contentContainer.style.marginLeft = `-${this.tabContent.offsetWidth * this.curIndex}px`;
+            this.host.style.setProperty('--content-width', `${this.tabContainer.offsetWidth}px`);
+            this.host.style.setProperty('--content-container-width', `${this.tabContainer.offsetWidth * this.tabs.length}px`);
+            this.indicator.style.setProperty('--indicator-width', `${width}px`);
+            this.indicator.style.setProperty('--indicator-left', `${left - this.tabContainerLeft + this.tabContainer.scrollLeft}px`);
+        });
+    }
+
+    handleClick(e) {
+        const tab = e.composedPath().find(el => el.classList.contains('tab'));
+        const {left} = tab.getBoundingClientRect();
+
+        this.indicator.style.setProperty('--indicator-left', `${left - this.tabContainerLeft + this.tabContainer.scrollLeft}px`);
+
+        this.tabs.forEach((t, index) => {
+            if(t === tab) {
+                t.classList.add('active');
+                this.contentContainer.style.marginLeft = `${0 - this.tabContent.offsetWidth * index}px`;
+                this.curIndex = index;
+            }
+            else {
+                t.classList.remove('active');
+            }
+
+        });
+    }
+}
+
+customElements.define('material-tabs', MaterialTabs);
--- wallaby.js
+++ wallaby.js
@@ -4,6 +4,7 @@ module.exports = () => {
             {pattern: 'node_modules/sinon/pkg/sinon.js', instrument: false, load: true},
             {pattern: 'node_modules/chai/chai.js', instrument: false, load: true},
             'src/material-table.js',
+            'src/material-tabs.js',
             {pattern: 'src/*.js', load: false}
         ],
         tests: ['test/*.test.js'],
