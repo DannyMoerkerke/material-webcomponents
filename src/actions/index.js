ddb82b2517de16f7235fee2b1f8baa70f589d866
Danny Moerkerke <danny@dannymoerkerke.nl>
2018-06-14 16:16:53 +0200

    removed redundant files

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export const INCREMENT = 'increment';
-export const DECREMENT = 'decrement';
-
-export const increment = amount => ({type: INCREMENT, amount});
-export const decrement = amount => ({type: DECREMENT, amount});
