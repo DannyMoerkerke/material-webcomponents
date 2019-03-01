6c238ae6399ac752b96c764ff01f19c38a5b9f39
Danny Moerkerke <danny@dannymoerkerke.nl>
2019-03-01 09:11:52 +0100

    fixed links

---
+++ demo/partials/index.js
@@ -0,0 +1,99 @@
+export const template = `<div class="intro">
+
+  <h1>Material Design Web Components</h1>
+
+  <p>
+    Material design implemented in native, cross-browser web components, no framework needed.
+  </p>
+
+  <div class="cards-row">
+    <div class="card">
+      <header>
+        <h3>
+          Standards based
+        </h3>
+      </header>
+      <section class="body">
+        <img src="/material-webcomponents/demo/assets/webcomponents.svg" style="width: 79px;">
+        <p>
+          All components consist of semantic HTML, scoped CSS and standard JavaScript.
+        </p>
+        <p>
+          No proprietary non-standard syntax so no build step is needed.
+        </p>
+      </section>
+    </div>
+
+    <div class="card">
+      <header>
+        <h3>
+          Cross-browser
+        </h3>
+      </header>
+      <section class="body">
+        <img src="/material-webcomponents/demo/assets/browsers.png">
+        <p>
+          Material Web Components run out of the box in all modern browsers
+        </p>
+        <p>
+          A polyfill is provided for outdated browsers.
+        </p>
+      </section>
+    </div>
+
+    <div class="card">
+      <header>
+        <h3>
+          Truly scoped CSS
+        </h3>
+      </header>
+      <section class="body">
+        <div class="scoped-css">
+          <p>[CSS]</p>
+        </div>
+        <p>
+          Material Web Components come with truly scoped CSS out of the box.
+        </p>
+        <p>
+          All CSS is contained inside each component and only applies to the component itself. No CSS-in-JS or other
+          non-standard solution needed.
+        </p>
+      </section>
+    </div>
+  </div>
+</div>
+
+<div class="getting-started">
+  <h2>Getting started</h2>
+  <p>
+    To install Material Web Components run:
+  </p>
+  <pre>
+    npm install material-webcomponents
+  </pre>
+  <p>
+    Then simply <code>import</code> the component you need in your script:
+  </p>
+  <pre>
+    import './node_modules/material-webcomponents/src/material-button.js';
+  </pre>
+  <p>
+    Or include it with a script tag as an ES6 module:
+  </p>
+  <pre>
+    &lt;script src="node_modules/material-webcomponents/src/material-button.js" type="module"&gt;&lt;/script&gt;
+  </pre>
+  <p>
+    Add the HTML tag:
+  </p>
+  <pre>
+    &lt;material-button label="Confirm" raised&gt;&lt;/material-button&gt;
+  </pre>
+  <p>
+    ...and you're in business!
+  </p>
+  <p>
+    Refer to the menu on the left to see documentation for each component.
+  </p>
+</div>
+`;
--- demo/routes.js
+++ demo/routes.js
@@ -1,219 +1,220 @@
 import router from '../src/router.js';
+import {template as index} from './partials/index.js';
 
 const outlet = document.querySelector('#content');
 
 router(outlet, [
-    {
-        url: '/material-webcomponents',
-        templateUrl: '../demo/partials/index.html'
-    },
-    {
-        url: '/material-webcomponents/material-app-bar',
-        templateUrl: '../demo/partials/material-app-bar.html'
-    },
-    {
-        url: '/material-webcomponents/material-button',
-        templateUrl: '../demo/partials/material-button.html'
-    },
-    {
-        url: '/material-webcomponents/material-checkbox',
-        templateUrl: '../demo/partials/material-checkbox.html'
-    },
-    {
-        url: '/material-webcomponents/material-datepicker',
-        templateUrl: '../demo/partials/material-datepicker.html',
-        controller() {
-            const picker = document.querySelector('material-datepicker');
-            const field = document.querySelector('material-textfield');
-            const dialog = document.querySelector('material-dialog');
-            const locale = 'en-EN';
+  {
+    url: '/material-webcomponents',
+    template: index
+  },
+  {
+    url: '/material-webcomponents/material-app-bar',
+    templateUrl: '../demo/partials/material-app-bar.html'
+  },
+  {
+    url: '/material-webcomponents/material-button',
+    templateUrl: '../demo/partials/material-button.html'
+  },
+  {
+    url: '/material-webcomponents/material-checkbox',
+    templateUrl: '../demo/partials/material-checkbox.html'
+  },
+  {
+    url: '/material-webcomponents/material-datepicker',
+    templateUrl: '../demo/partials/material-datepicker.html',
+    controller() {
+      const picker = document.querySelector('material-datepicker');
+      const field = document.querySelector('material-textfield');
+      const dialog = document.querySelector('material-dialog');
+      const locale = 'en-EN';
 
-            field.addEventListener('focus', dialog.open.bind(dialog));
+      field.addEventListener('focus', dialog.open.bind(dialog));
 
-            picker.addEventListener('change', e => {
-                field.value = e.detail.formattedDate;
-                dialog.close();
-            });
+      picker.addEventListener('change', e => {
+        field.value = e.detail.formattedDate;
+        dialog.close();
+      });
 
-            picker.addEventListener('close', dialog.close.bind(dialog));
+      picker.addEventListener('close', dialog.close.bind(dialog));
 
-            const formattedDate = new Intl.DateTimeFormat(locale, {
-                day: '2-digit',
-                month: '2-digit',
-                year: 'numeric'
-            }).format(new Date());
+      const formattedDate = new Intl.DateTimeFormat(locale, {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      }).format(new Date());
 
-            picker.date = formattedDate;
-            field.value = formattedDate;
-        }
-    },
-    {
-        url: '/material-webcomponents/material-dialog',
-        templateUrl: '../demo/partials/material-dialog.html',
-        controller() {
-            const standardDialog = document.querySelector('#standard-dialog');
-            const modalDialog = document.querySelector('#modal-dialog');
-
-            document.querySelector('#open-standard').addEventListener('click', standardDialog.open.bind(standardDialog));
-            document.querySelector('#close-standard').addEventListener('click', standardDialog.close.bind(standardDialog));
-            document.querySelector('#open-modal').addEventListener('click', modalDialog.open.bind(modalDialog));
-            document.querySelector('#close-modal').addEventListener('click', modalDialog.close.bind(modalDialog));
-        }
-    },
-    {
-        url: '/material-webcomponents/material-dropdown',
-        templateUrl: '../demo/partials/material-dropdown.html'
-    },
-    {
-        url: '/material-webcomponents/material-progress',
-        templateUrl: '../demo/partials/material-progress.html'
-    },
-    {
-        url: '/material-webcomponents/material-drawer',
-        templateUrl: '../demo/partials/material-drawer.html',
-        controller() {
-            const button = document.querySelector('#material-drawer-toggle');
-            const drawer = document.querySelector('#demo-material-drawer');
-
-            button.addEventListener('click', drawer.toggle.bind(drawer));
-        }
-    },
-    {
-        url: '/material-webcomponents/material-radiobutton',
-        templateUrl: '../demo/partials/material-radiobutton.html'
-    },
-    {
-        url: '/material-webcomponents/material-slider',
-        templateUrl: '../demo/partials/material-slider.html'
-    },
-    {
-        url: '/material-webcomponents/material-switch',
-        templateUrl: '../demo/partials/material-switch.html'
-    },
-    {
-        url: '/material-webcomponents/material-table',
-        templateUrl: '../demo/partials/material-table.html',
-        controller() {
-            customElements.whenDefined('material-table')
-            .then(() => {
-                const tables = document.querySelectorAll('material-table');
-                tables[0].data = [
-                    {
-                        id: 1,
-                        language: 'Javascript',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 2,
-                        language: 'PHP',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 3,
-                        language: 'Scala',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 4,
-                        language: 'CSS',
-                        scope: 'Frontend'
-                    }
-                ];
-                tables[1].data = [
-                    {
-                        id: 1,
-                        language: 'Javascript',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 2,
-                        language: 'PHP',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 3,
-                        language: 'Scala',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 4,
-                        language: 'CSS',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 5,
-                        language: 'Javascript',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 6,
-                        language: 'PHP',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 7,
-                        language: 'Scala',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 8,
-                        language: 'CSS',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 9,
-                        language: 'Javascript',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 10,
-                        language: 'PHP',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 11,
-                        language: 'Scala',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 12,
-                        language: 'CSS',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 13,
-                        language: 'Javascript',
-                        scope: 'Frontend'
-                    },
-                    {
-                        id: 14,
-                        language: 'PHP',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 15,
-                        language: 'Scala',
-                        scope: 'Backend'
-                    },
-                    {
-                        id: 16,
-                        language: 'CSS',
-                        scope: 'Frontend'
-                    }
-                ];
-            });
-        }
-    },
-    {
-        url: '/material-webcomponents/material-tabs',
-        templateUrl: '../demo/partials/material-tabs.html'
-    },
-    {
-        url: '/material-webcomponents/material-textfield',
-        templateUrl: '../demo/partials/material-textfield.html'
-    },
-    {
-        url: '/material-webcomponents/material-slidemenu',
-        templateUrl: '../demo/partials/material-slidemenu.html'
+      picker.date = formattedDate;
+      field.value = formattedDate;
     }
+  },
+  {
+    url: '/material-webcomponents/material-dialog',
+    templateUrl: '../demo/partials/material-dialog.html',
+    controller() {
+      const standardDialog = document.querySelector('#standard-dialog');
+      const modalDialog = document.querySelector('#modal-dialog');
+
+      document.querySelector('#open-standard').addEventListener('click', standardDialog.open.bind(standardDialog));
+      document.querySelector('#close-standard').addEventListener('click', standardDialog.close.bind(standardDialog));
+      document.querySelector('#open-modal').addEventListener('click', modalDialog.open.bind(modalDialog));
+      document.querySelector('#close-modal').addEventListener('click', modalDialog.close.bind(modalDialog));
+    }
+  },
+  {
+    url: '/material-webcomponents/material-dropdown',
+    templateUrl: '../demo/partials/material-dropdown.html'
+  },
+  {
+    url: '/material-webcomponents/material-progress',
+    templateUrl: '../demo/partials/material-progress.html'
+  },
+  {
+    url: '/material-webcomponents/material-drawer',
+    templateUrl: '../demo/partials/material-drawer.html',
+    controller() {
+      const button = document.querySelector('#material-drawer-toggle');
+      const drawer = document.querySelector('#demo-material-drawer');
+
+      button.addEventListener('click', drawer.toggle.bind(drawer));
+    }
+  },
+  {
+    url: '/material-webcomponents/material-radiobutton',
+    templateUrl: '../demo/partials/material-radiobutton.html'
+  },
+  {
+    url: '/material-webcomponents/material-slider',
+    templateUrl: '../demo/partials/material-slider.html'
+  },
+  {
+    url: '/material-webcomponents/material-switch',
+    templateUrl: '../demo/partials/material-switch.html'
+  },
+  {
+    url: '/material-webcomponents/material-table',
+    templateUrl: '../demo/partials/material-table.html',
+    controller() {
+      customElements.whenDefined('material-table')
+      .then(() => {
+        const tables = document.querySelectorAll('material-table');
+        tables[0].data = [
+          {
+            id: 1,
+            language: 'Javascript',
+            scope: 'Frontend'
+          },
+          {
+            id: 2,
+            language: 'PHP',
+            scope: 'Backend'
+          },
+          {
+            id: 3,
+            language: 'Scala',
+            scope: 'Backend'
+          },
+          {
+            id: 4,
+            language: 'CSS',
+            scope: 'Frontend'
+          }
+        ];
+        tables[1].data = [
+          {
+            id: 1,
+            language: 'Javascript',
+            scope: 'Frontend'
+          },
+          {
+            id: 2,
+            language: 'PHP',
+            scope: 'Backend'
+          },
+          {
+            id: 3,
+            language: 'Scala',
+            scope: 'Backend'
+          },
+          {
+            id: 4,
+            language: 'CSS',
+            scope: 'Frontend'
+          },
+          {
+            id: 5,
+            language: 'Javascript',
+            scope: 'Frontend'
+          },
+          {
+            id: 6,
+            language: 'PHP',
+            scope: 'Backend'
+          },
+          {
+            id: 7,
+            language: 'Scala',
+            scope: 'Backend'
+          },
+          {
+            id: 8,
+            language: 'CSS',
+            scope: 'Frontend'
+          },
+          {
+            id: 9,
+            language: 'Javascript',
+            scope: 'Frontend'
+          },
+          {
+            id: 10,
+            language: 'PHP',
+            scope: 'Backend'
+          },
+          {
+            id: 11,
+            language: 'Scala',
+            scope: 'Backend'
+          },
+          {
+            id: 12,
+            language: 'CSS',
+            scope: 'Frontend'
+          },
+          {
+            id: 13,
+            language: 'Javascript',
+            scope: 'Frontend'
+          },
+          {
+            id: 14,
+            language: 'PHP',
+            scope: 'Backend'
+          },
+          {
+            id: 15,
+            language: 'Scala',
+            scope: 'Backend'
+          },
+          {
+            id: 16,
+            language: 'CSS',
+            scope: 'Frontend'
+          }
+        ];
+      });
+    }
+  },
+  {
+    url: '/material-webcomponents/material-tabs',
+    templateUrl: '../demo/partials/material-tabs.html'
+  },
+  {
+    url: '/material-webcomponents/material-textfield',
+    templateUrl: '../demo/partials/material-textfield.html'
+  },
+  {
+    url: '/material-webcomponents/material-slidemenu',
+    templateUrl: '../demo/partials/material-slidemenu.html'
+  }
 ]);
